f0bf3188fa794884e74c0b47d336f21c443af06f
pronchaia <pronchai.a@outlook.com>
2019-03-19 17:36:40 +0700

    Init Function list

---
+++ Functional/Functional.List.Exp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Functional.List.Exp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
