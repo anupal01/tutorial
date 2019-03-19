9c8afedda0ba0fc270830bc98938b36496eca560
pronchaia <pronchai.a@outlook.com>
2019-03-19 17:25:04 +0700

    AsyncProgramming-Init

---
+++ AsyncProgramming/Concurrent/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+namespace Concurrent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId}");
+            var t = new Thread(WirteY);
+            t.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("X");
+            }
+        }
+
+        static void WirteY()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("Y");
+            }
+        }
+    }
+}
